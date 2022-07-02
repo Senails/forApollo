5466b5088eac2b04411944bd2de612f9af384a11
Senails <76501177+Senails@users.noreply.github.com>
2022-07-02 21:08:56 +0300

    block1

--- src/pages/main/blocks/block1/Block1.tsx
+++ src/pages/main/blocks/block1/Block1.tsx
@@ -5,7 +5,14 @@ export default function Block1():JSX.Element {
     return <section className="block1">
         <div className="content">
             <div className="foranimation"></div>
-            <div className="fortext"></div>
+            <div className="fortext">
+                <h1>Lorem Ipsum Sans-Serif</h1>
+                <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
+                <div className="buttons">
+                    <a href=""><div className="disc"></div></a>
+                    <a href=""><div className="conect">Сотрудничество</div></a>
+                </div>
+            </div>
         </div>
     </section>
 }
