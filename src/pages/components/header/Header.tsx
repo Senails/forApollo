0ea49105d8772d0a0453a5fb41d59578e7b59bb8
Senails <76501177+Senails@users.noreply.github.com>
2022-07-02 16:29:19 +0300

    header

--- src/pages/components/header/Header.tsx
+++ src/pages/components/header/Header.tsx
@@ -3,12 +3,18 @@ import './header.css'
 
 export default function Header():JSX.Element {
     return <header>
-            <div className="logo"></div>
+            <div className="logo">
+                <div className="log"></div>
+                <p>ApolloCode Studio</p>
+            </div>
             <nav>
                 <a href="">Направления</a>
                 <a href="">Команда</a>
                 <a href="">Поддержка</a>
             </nav>
-            <div className=""></div>
+            <div className="telegadiscord">
+                <a href=""><div></div></a>
+                <a href=""><div></div></a>
+            </div>
     </header>
 }
